e9730cb4b76fc12f0ec96addc380614110e05d0a
Zachri Hall <hallzachri@gmail.com>
2022-06-16 15:15:02 -0400

    added the bare minimum html and styling for a show. sticking witht he ahtml/css first approacht then translate to js

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,55 @@
+// const ronaldLane = {
+//     'date': "Mon Sept 06 2021",
+//     'venue': "Ronald Lane",
+//     'location': "San Francisco, CA"
+// }
+
+// const pier3East = {
+//     'date': "Tue Sept 21 2021",
+//     'venue': "Pier 3 East",
+//     'location': "San Francisco, CA"
+// }
+
+// const viewLounge = {
+//     'date': "Fri Oct 15 2021",
+//     'venue': "View Lounge",
+//     'location': "San Francisco, CA"
+// }
+
+// const hyattAgency = {
+//     'date': "Sat Nove 06 2021",
+//     'venue': "Hyatt Agency",
+//     'location': "San Francisco, CA"
+// }
+
+// const moscowCenter = {
+//     'date': "Fri Nov 26 2021",
+//     'venue': "Moscow Center",
+//     'location': "San Francisco, CA"
+// }
+
+// const pressClub = {
+//     'date': "WEd Dec 15 2021",
+//     'venue': "Press Club",
+//     'location': "San Francisco, CA"
+// }
+
+
+// const showsContainer = document.querySelector(".shows-container");
+
+// const sanfranConatainer = document.createElement("ul");
+// sanfranConatainer.classList.add("shows-container__show--san-fran");
+
+// const sanFranDateLabel = document.createElement("li");
+// sanFranDateLabel.classList.add("shows-container__show--san-fran-date-label");
+// sanFranDateLabel.textContent = ronaldLane.date;
+
+
+
+
+// //APPENDING
+// showsContainer.append(sanfranConatainer);
+// sanfranConatainer.append(sanFranDateLabel);
+
+
+
