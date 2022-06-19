ecdd533588902735d202ed4c2fa0b457d206681c
Zachri Hall <hallzachri@gmail.com>
2022-06-19 18:35:38 -0400

    added default shows and comments

---
+++ scripts/index-page.js
@@ -0,0 +1,8 @@
+// 1. create the variables using the DOM API
+const userName = document.querySelector;
+
+// 2. Assign the variables to objects using the same structure 
+
+// 3. Store the new comment object in an array
+
+// 4. have a function called displayComment() to loop through the objects in the array and display them
